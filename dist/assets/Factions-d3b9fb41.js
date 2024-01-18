3adcafb1c2f7046eb304a545eaf35f511629a501
Thomas Salvi <thomaspsalvi@gmail.com>
2024-01-17 22:45:35 -0600

    dist!

---
+++ dist/assets/Factions-d3b9fb41.js
@@ -0,0 +1,1 @@
+var r=(e=>(e.Atreides="Atreides",e.Harkonnen="Harkonnen",e.Emperor="Emperor",e.SpacingGuild="SpacingGuild",e.Fremen="Fremen",e.BeneGesserit="BeneGesserit",e.Ixians="Ixians",e.Tleilaxu="Tleilaxu",e.Richese="Richese",e.CHOAM="CHOAM",e.Ecaz="Ecaz",e.Moritani="Moritani",e.Unknown="n/a",e))(r||{});export{r as F};
