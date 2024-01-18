f5ce23709d109adfd23d1c3f43f200bb457ca5d6
Thomas Salvi <thomaspsalvi@gmail.com>
2024-01-17 23:36:47 -0600

    gasg

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/GameOfGames/',
+  base: '/GameOfGames',
   plugins: [
     vue({
       template: { transformAssetUrls },
