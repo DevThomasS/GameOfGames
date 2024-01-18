69ae878189270a6d4ba890261c7672ea029d4ca6
Thomas Salvi <thomaspsalvi@gmail.com>
2024-01-17 23:24:26 -0600

    oh please oh please

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/GameOfGames/',
   plugins: [
     vue({
       template: { transformAssetUrls },
