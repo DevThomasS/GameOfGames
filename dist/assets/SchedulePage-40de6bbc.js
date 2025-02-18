6ee10656d820d0046fec879022cd31121e38fe3d
DevThomasS <31936811+DevThomasS@users.noreply.github.com>
2025-02-18 16:46:15 -0600

    zwm2

---
+++ dist/assets/SchedulePage-40de6bbc.js
@@ -0,0 +1,1 @@
+import{d as s,_ as o,o as c,b as t,p as a,j as d,e as _}from"./index-19890962.js";const n=s({});const p=e=>(a("data-v-bfed57ec"),e=e(),d(),e),l={class:"default-page"},r=p(()=>_("div",{class:"dune-box"},null,-1)),i=[r];function u(e,f,h,v,b,m){return c(),t("div",l,i)}const S=o(n,[["render",u],["__scopeId","data-v-bfed57ec"]]);export{S as default};
